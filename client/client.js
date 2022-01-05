9862676c425ea09b72d2a386ed407f4be447d222
LEANG Denis <denis.leang@gmail.com>
2022-01-05 21:52:31 +0100

    Added username handling

--- client/client.js
+++ client/client.js
@@ -1,24 +1,44 @@
 // Loading socket variable
 var socket = io();
 
-// Loading chat container content
-let form = document.getElementById("chat-form");
-let messages = document.getElementById("chat-messages");
-let input = document.getElementById("chat-input");
-let button = document.getElementById("chat-button");
+// Loading login container content 
+let login_form = document.getElementById("login-form");
+let login_input = document.getElementById("login-input");
 
-// Handling chat input value
-form.addEventListener('submit', function(e) {
+// Loading chat container content
+let chat_form = document.getElementById("chat-form");
+let chat_messages = document.getElementById("chat-messages");
+let chat_input = document.getElementById("chat-input");
+
+// Handling login input value
+login_form.addEventListener('submit', function(e) {
   e.preventDefault();
-  if (input.value) {
-    socket.emit('chat-message', input.value);
-    input.value = '';
+
+  var user = {
+    username: login_input.value
+  }
+
+  if (user.username && user.username.length < 30) {
+      socket.emit('login-user', user);
+      document.getElementById("login-overlay").remove();
+  }
+    else {
+      alert("Enter a name between 0 and 30 characters");
   }
 });
 
-// Handling chat messages
+// Handling chat input value
+chat_form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  if (chat_input.value) {
+    socket.emit('chat-message', chat_input.value);
+    chat_input.value = '';
+  }
+});
+
+// Recieving chat messages
 socket.on('chat-message', function(message) {
   var item = document.createElement('li');
   item.textContent = message;
-  messages.appendChild(item);
+  chat_messages.appendChild(item);
 });
--- server.js
+++ server.js
@@ -8,22 +8,34 @@ const app = express()
 const server = http.Server(app)
 const io = socketIO(server)
 
+// Handling HTTP requests
 app.use("/", express.static(__dirname + "/"));
 app.use("/", express.static(__dirname + "/client"));
 
+// Launching server on port 3000
 server.listen(3000, function(){
   console.log('Server is listening on *:3000');
 });
 
 // Creating socket io event 
 io.on('connection', function(socket){
-  console.log('User is with us');
+  
+  var loggedUser;
+
+  // Handling user connection
+  socket.on('login-user', function (loggedUser) {
+    console.log(loggedUser.username + " is with us");
+    user = loggedUser;
+  });
   
   socket.on('disconnect', function(){
     console.log('User left us');
   });
 
-  socket.on('chat-message', (message) => {
+  // Handling 'chat message' event 
+  socket.on('chat-message', function (message) {
+    message.username = user.username;
     io.emit('chat-message', message);
+    console.log('Message de : ' + user.username);
   });
 });
