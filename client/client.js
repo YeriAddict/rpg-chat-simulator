3af446f80005b36847598db026ba43e1d4e3d471
LEANG Denis <denis.leang@gmail.com>
2022-01-06 01:27:46 +0100

    Added connected users on the left

--- client/client.js
+++ client/client.js
@@ -6,33 +6,39 @@ var socket = io();
 
 // Handling login input value
 $('#login form').submit(function (e) {
- e.preventDefault();
+  e.preventDefault();
 
- var user = {
-   username : $('#login input').val().trim()
- };
+  var user = {
+    username : $('#login input').val().trim()
+  };
 
- if (user.username.length > 0 && user.username.length < 30) { 
-   socket.emit('user-login', user);
-   $('#login').remove(); 
- }
- else {
-   alert("Enter a name between 0 and 30 characters")
- }
+  if (user.username.length > 0 && user.username.length < 30) { 
+    socket.emit('user-login', user, function (success) {
+      if (success) {
+        $('#login').remove(); 
+      }
+      else {
+        alert("This name is already taken");
+      }
+    });
+  }
+  else {
+    alert("Enter a name between 0 and 30 characters");
+  }
 });
 
 // Handling chat input value
 $('#chat-form').submit(function (e) {
- e.preventDefault();
+  e.preventDefault();
 
- var message = {
-   text : $('#chat-input').val()
- };
+  var message = {
+    text : $('#chat-input').val()
+  };
 
- $('#chat-input').val('');
- if (message.text.trim().length !== 0) { 
-   socket.emit('chat-message', message);
- }
+$('#chat-input').val('');
+  if (message.text.trim().length !== 0) { 
+    socket.emit('chat-message', message);
+  }
 });
 
 // Adding messages to the chatbox
@@ -45,4 +51,18 @@ socket.on('chat-message', function (message) {
 socket.on('service-message', function (message) {
   $('#chat-messages').append($('<li>').html('<span class="broadcast">[MINICHAT]</span> ' + message.text));
   $("#chat-messages").scrollTop($("#chat-messages")[0].scrollHeight)
+});
+
+// Handling user connection (for the users list)
+socket.on('user-login', function (user) {
+  $('#users').append($('<li>').html(user.username));
+  setTimeout(function () {
+    $('#users li.new').removeClass('new');
+  }, 1000);
+});
+
+// Handling user disconnection (for the users list)
+socket.on('user-logout', function (user) {
+  var selector = '#users li.' + user.username;
+  $(selector).remove();
 });
--- server.js
+++ server.js
@@ -17,32 +17,64 @@ server.listen(3000, function(){
   console.log('Server is listening on *:3000');
 });
 
+// Creating list of connected users
+var users = [];
+
 // Creating socket io event 
 io.on('connection', function (socket) {
 
   var loggedUser;
 
   // Handling connection event and broadcast message
-  socket.on('user-login', function (user) {
-    loggedUser = user;
-    if (loggedUser !== undefined) {
-      var serviceMessage = {
+  socket.on('user-login', function (user, callback) {
+    // Checking if user exists
+    var userIndex = -1;
+    for (i = 0; i < users.length; i++) {
+      if (users[i].username === user.username) {
+        userIndex = i;
+      }
+    }
+    if (user !== undefined && userIndex === -1) { 
+      // Adding user to the list
+      loggedUser = user;
+      users.push(loggedUser);
+      
+      var userServiceMessage = {
+        text: "You logged in as " + loggedUser.username,
+        type: 'login'
+      };
+      var broadcastedServiceMessage = {
         text: loggedUser.username + " is with us",
         type: 'login'
       };
-      socket.broadcast.emit('service-message', serviceMessage);
+      socket.emit('service-message', userServiceMessage);
+      socket.broadcast.emit('service-message', broadcastedServiceMessage);
+      // Emission de 'user-login' et appel du callback
+      io.emit('user-login', loggedUser);
+      callback(true);
+    } else {
+      callback(false);
     }
   });
 
-// Handling disconnection event and broadcast message
+  // Handling disconnection event and broadcast message
   socket.on('disconnect', function () {
     if (loggedUser !== undefined) {
-      console.log('user disconnected : ' + loggedUser.username);
+
+      // Announcement message
       var serviceMessage = {
         text: loggedUser.username + " left us",
         type: 'logout'
       };
       socket.broadcast.emit('service-message', serviceMessage);
+
+      // Deleting disconnected users
+      var userIndex = users.indexOf(loggedUser);
+      if (userIndex !== -1) {
+        users.splice(userIndex, 1);
+      }
+      // Sending user-logout with disconnected user
+      io.emit('user-logout', loggedUser);
     }
   });
 
@@ -51,4 +83,9 @@ io.on('connection', function (socket) {
     message.username = loggedUser.username;
     io.emit('chat-message', message);
   });
+
+  // Sending users list to everyone
+  for (i = 0; i < users.length; i++) {
+    socket.emit('user-login', users[i]);
+  }
 });
